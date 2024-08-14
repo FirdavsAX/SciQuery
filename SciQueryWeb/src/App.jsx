4d97348a4ba56a6b79d22a40f9d93c1887f51a6f
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-08-14 19:06:14 +0500

    Added Layout

--- SciQueryWeb/src/App.jsx
+++ SciQueryWeb/src/App.jsx
@@ -7,74 +7,87 @@ import About from "./pages/About/About";
 import RootLayout from "./layout/RootLayout";
 import QuestionPage from "./pages/QuestionPage/QuestionPage";
 import ContactLayout from "./layout/ContactLayout";
+import AuthLayout from "./layout/AuthLayout";
 import Faq from "./pages/help/Faq";
 import Form from "./pages/help/Form";
 import QuestionsList from "./components/Questions/QuestionsList/QuestionsList";
 import QuestionDetail from "./components/Questions/QuestionDetail/QuestionDetail";
 import ErrorPage from "./pages/help/ErrorPage";
 import CreateAndUpdateQuestionPage from "./pages/QuestionPage/Create/CreateAndUpdateQuestionPage";
-import { lazy, useState } from "react";
+import { useState } from "react";
 import Login from "./pages/Auth/Login";
-
+import Register from "./pages/Auth/Register";
 
 
 function App() {
-  const [token,setToken] = useState(window.localStorage.getItem('token'));
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
   const routes = createBrowserRouter([
-    token ? 
-    {
-      path: "/",
-      errorElement : <ErrorPage/>,
-      element: <RootLayout />,
-      children: [
-        {
-          index: true,
-          element: <Home />
-        },
-        {
-          path: "/about",
-          element: <About />,
-        },
-        {
-          path: "/questions",
-          element: <QuestionPage />,
+    token
+      ? {
+          path: "/",
+          errorElement: <ErrorPage />,
+          element: <RootLayout />,
           children: [
             {
               index: true,
-              element: <QuestionsList />,
+              element: <Home />,
             },
             {
-              path: ":id",
-              element: <QuestionDetail />,
+              path: "/about",
+              element: <About />,
+            },
+            {
+              path: "/questions",
+              element: <QuestionPage />,
+              children: [
+                {
+                  index: true,
+                  element: <QuestionsList />,
+                },
+                {
+                  path: ":id",
+                  element: <QuestionDetail />,
+                },
+              ],
+            },
+            {
+              path: "new",
+              element: <CreateAndUpdateQuestionPage />,
+            },
+            {
+              path: "/contact/",
+              element: <ContactLayout />,
+              children: [
+                {
+                  path: "faq",
+                  element: <Faq />,
+                },
+                {
+                  path: "form",
+                  element: <Form />,
+                },
+              ],
             },
           ],
-        },
-        {
-          path : "new",
-          element : <CreateAndUpdateQuestionPage/>
-        },
-        {
-          path: "/contact/",
-          element: <ContactLayout />,
+        }
+      : {
+          element: <AuthLayout setToken={setToken} />,
+          action: () => <AuthLayout setToken={setToken} />,
+          path: "/",
           children: [
             {
-              path: "faq",
-              element: <Faq />,
+              index:true,
+              element: <Register setToken={setToken} />,
+              action: () => <Register setToken={setToken} />,
+              path: "register",
             },
             {
-              path: "form",
-              element: <Form />,
+              element: <Login setToken={setToken} />,
+              action: () => <Login setToken={setToken} />,
+              path: "/",
             },
           ],
         },
-      ],
-    }
-    :
-    {
-      element : <Login setToken={setToken}/>,
-      action : () => <Login setToken={setToken}/>,
-      path : '/'
-    }
   ]);
   return (
     <div className="App">
