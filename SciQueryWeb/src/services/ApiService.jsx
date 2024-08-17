03712f558570f297eff57631b0e23caf5c803a3b
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-08-17 14:13:23 +0500

    EDITED

--- SciQueryWeb/src/services/ApiService.jsx
+++ SciQueryWeb/src/services/ApiService.jsx
@@ -1,14 +1,12 @@
 import axios from "axios";
-import { API_BASE_URL, TOKEN } from "../config/Constants";
+import { API_BASE_URL } from "../config/Constants";
 
 export const postData = async (url, data) => {
   url = API_BASE_URL + url;
   try {
     const response = await axios.post(url, data, {
       headers: {
-        // Do not manually set 'Content-Type' when sending FormData
-        Authorization: TOKEN ? `Bearer ${TOKEN}` : "",
-        // The browser will set it automatically along with the boundary
+        Authorization: window.localStorage.getItem('token') ? `Bearer ${window.localStorage.getItem('token')}` : "",
       },
     });
     return response.data;
